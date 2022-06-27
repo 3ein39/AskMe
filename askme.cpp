3827335661448955085a1f241c3172b884689d8d
HusseinHany <hhany3997@gmail.com>
2022-06-27 15:19:46 +0200

    create basic user struct

--- askme.cpp
+++ askme.cpp
@@ -1,7 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+struct User {
+    string user_name {};
+    string password {};
+    string name {};
+    string email {};
+    bool allow_aq {};
+};
+vector<User> users;
+
+
+
+
+
 int main() {
     cout << "Hello world!";
     return 0;
 }
+
+/*
+ * each user has name , password , email , allow AQ
+ */
