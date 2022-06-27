45b67a9ca5405b39dafb498069090dc7bf72eb66
HusseinHany <hhany3997@gmail.com>
2022-06-27 15:30:32 +0200

    create login/signup menu

--- askme.cpp
+++ askme.cpp
@@ -10,11 +10,38 @@ struct User {
 };
 vector<User> users;
 
+int menu() {
+    int choice = -1;
+    while (choice == -1) {
+        cout << "Menu:" << endl;
+        cout << '\t' <<"1: Login\n";
+        cout << '\t' <<"2: Sign Up\n";
+
+        cin >> choice;
+
+        if (!(1 <= choice && choice <= 2)) {
+            cout << "Invalid choice. Try again\n";
+            choice = -1;	// loop keep working
+        }
+    }
+    return choice;
+}
 
 
 
+void system() {
+    while (true) {
+        int choice = menu();
+
+        /*if (choice == 1)
+            signIn();
+        else
+            signUp();*/
+    }
+}
 
 int main() {
+    system();
     cout << "Hello world!";
     return 0;
 }
